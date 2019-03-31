04665fd4409c55a63193ba0636eef81c81702c6a
shivi123gangwar <32398611+shivi123gangwar@users.noreply.github.com>
2019-03-31 23:15:20 +0530

    Create model.js

---
+++ Calculator/model.js
@@ -0,0 +1,8 @@
+var calcOperations = {
+    add(firstNum, secondNum){
+        return parseInt(firstNum)+ parseInt(secondNum);
+    },
+    subtract(firstNum, secondNum){
+       return parseInt(firstNum) - parseInt(secondNum);
+}
+}
